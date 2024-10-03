898828b2dcf89181ced8149cbc209c3027304e0b
Andres Argote <andresargote2019@gmail.com>
2024-10-03 15:00:13 +0200

    Fixed upload issues

--- src/app/api/signature/route.ts
+++ src/app/api/signature/route.ts
@@ -1,8 +1,7 @@
-import { NextApiRequest, NextApiResponse } from 'next';
 import { NextResponse } from 'next/server';
 import { v2 as cloudinary } from 'cloudinary';
 
-export async function GET(req: NextApiRequest, res: NextApiResponse) {
+export async function GET() {
   const timestamp = Math.round(Date.now() / 1000);
 
   const apiSecret = process.env.NEXT_CLOUDINARY_API_SECRET ?? '';
@@ -14,7 +13,7 @@ export async function GET(req: NextApiRequest, res: NextApiResponse) {
     apiSecret
   );
 
-  return NextResponse.json({
+  return Response.json({
     signature,
     timestamp,
     api_key: process.env.NEXT_CLOUDINARY_API_KEY,
